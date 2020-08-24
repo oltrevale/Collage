bb599bf9b34ac0bd89c13b59f7fbaf9eda61cf18
K <valeriocampanella1998@gmail.com>
2020-08-24 20:40:42 +0200

    fix

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ import sys
 vid = cv2.VideoCapture(sys.argv[1])
 vid.set(cv2.CAP_PROP_POS_AVI_RATIO, 1)
 
-print(vid.get(cv2.CAP_PROP_POS_MSEC))
 num_frames = int(vid.get(cv2.CAP_PROP_FRAME_COUNT))
 height = vid.get(cv2.CAP_PROP_FRAME_HEIGHT)
 width = vid.get(cv2.CAP_PROP_FRAME_WIDTH)
+#todo trovare un algoritmo per cercare il numero di frame ci servono in base alla lunghezza del video
 chunk = int(num_frames / 401)
 list_images = []
 for i in range(chunk, num_frames - chunk, chunk):
